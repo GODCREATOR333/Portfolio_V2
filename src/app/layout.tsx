bad248ccfd1d20e2504c743af88211e5ae1c2359
GODCREATOR333 <haripreetham.jntuh@gmail.com>
2026-01-09 03:16:05 +0530

    Added analytics msft clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,15 +2,13 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-
   title: "Hari Preetham - Portfolio",
   description: "Hari Preetham Portfolio",
-    icons: {
+  icons: {
     icon: "/favicon.ico",
     shortcut: "/favicon.ico",
     apple: "/apple-touch-icon.png",
   },
-
 };
 
 export default function RootLayout({
@@ -25,6 +23,19 @@ export default function RootLayout({
         <link
           href="https://fonts.googleapis.com/css?family=Zilla+Slab:400,700|Nunito+Sans:400,600,700&display=swap"
           rel="stylesheet"
+        />
+        {/* Microsoft Clarity */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "uybuxlarrm");
+            `,
+          }}
         />
       </head>
       <body suppressHydrationWarning={true}>
